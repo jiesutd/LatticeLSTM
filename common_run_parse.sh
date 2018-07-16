e9eb28e97026c23a9b76c955781faa67b36e3fa0
FAREAST\yueych <daniyuu@163.com>
2018-07-16 17:24:10 +0800

    update scripts

---
+++ common_run_parse.sh
@@ -0,0 +1,5 @@
+python main.py --status decode \
+		--raw ./data/CommonNER/common.test.char \
+		--savedset ./data/CommonNER/common.dset \
+		--loadmodel ./data/CommonNER/common.0.model \
+		--output ./data/CommonNER/common.raw.out \
--- run_demo.sh
+++ run_demo.sh
@@ -6,7 +6,7 @@ python main.py --status train \
 
 
 # python main.py --status decode \
-# 		--raw ../data/onto4ner.cn/demo.test.char \
-# 		--savedset ../data/onto4ner.cn/demo.dset \
-# 		--loadmodel ../data/onto4ner.cn/demo.0.model \
-# 		--output ../data/onto4ner.cn/demo.raw.out \
+# 		--raw ./data/onto4ner.cn/demo.test.char \
+# 		--savedset ./data/onto4ner.cn/demo.dset \
+# 		--loadmodel ./data/onto4ner.cn/demo.0.model \
+# 		--output ./data/onto4ner.cn/demo.raw.out \
