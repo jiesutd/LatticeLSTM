af467d2be667d18afe4eacf46fe16b13e4f764b4
FAREAST\yueych <yueych@microsoft.com>
2018-07-17 00:05:19 +0000

    demo parse

---
+++ demo_run_parse.sh
@@ -0,0 +1,5 @@
+python main.py --status decode \
+         --raw ./data/demo.test.char \
+         --savedset ./data/demo.dset \
+         --loadmodel ./data/demo.0.model \
+          --output ./data/demo.raw.out \
