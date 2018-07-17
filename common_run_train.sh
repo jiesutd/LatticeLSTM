ea70b3b9bfdf8a1a4875c162cf69caa47e101706
FAREAST\yueych <yueych@microsoft.com>
2018-07-17 01:28:55 +0000

    change training scripts

--- common_run_train.sh
+++ common_run_train.sh
@@ -1,5 +1,1 @@
-python main.py --status train \
-		--train ./data/CommonNER/train.txt \
-		--dev ./data/CommonNER/validation.txt \
-		--test ./data/CommonNER/test.txt \
-		--savemodel ./data/CommonNER/common \
+python main.py --status train --train ./data/CommonNER/train.txt --dev ./data/CommonNER/validation.txt --test ./data/CommonNER/test.txt --savemodel ./data/CommonNER/common
